9b3848cb31d6e9b576e519f09f138e784dd763da
PedroJimenez <pedro.jimenezli@anahuac.mx>
2023-11-24 19:22:28 -0600

    Carga Inicial y Configuracion de Iconos y "Bienvenida"

---
+++ app/src/main/java/com/example/biblioteca/Bienvenida.kt
@@ -0,0 +1,31 @@
+package com.example.biblioteca
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+
+class Bienvenida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bienvenida)
+        VerBienvenida()
+    }
+
+    fun VerBienvenida(){
+        object : CountDownTimer(2000,1000){
+            override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish() {
+                //Vamos hacia el MainActivity
+                val intent = Intent(this@Bienvenida, MainActivity::class.java)
+                startActivity(intent)
+                finishAffinity()
+            }
+
+        }.start()
+    }
+
+}
