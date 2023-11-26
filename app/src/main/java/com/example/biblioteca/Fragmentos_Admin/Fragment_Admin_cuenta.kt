0222b032ce1280d6e4c9880e62490be1cbacc6dc
PedroJimenez <pedro.jimenezli@anahuac.mx>
2023-11-26 10:43:30 -0600

    Cerrar sesion admin

--- app/src/main/java/com/example/biblioteca/Fragmentos_Admin/Fragment_Admin_cuenta.kt
+++ app/src/main/java/com/example/biblioteca/Fragmentos_Admin/Fragment_Admin_cuenta.kt
@@ -1,20 +1,44 @@
 package com.example.biblioteca.Fragmentos_Admin
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.biblioteca.Eligir_rol
 import com.example.biblioteca.R
+import com.example.biblioteca.databinding.FragmentAdminCuentaBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.core.Context
 
 
 class Fragment_Admin_cuenta : Fragment() {
 
+    private lateinit var binding : FragmentAdminCuentaBinding
+    private lateinit var firebaseAuth : FirebaseAuth
+    private lateinit var mContext : Context
+
+    override fun onAttach(context: android.content.Context) {
+        //mContext = context
+        super.onAttach(context)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__admin_cuenta, container, false)
+        binding = FragmentAdminCuentaBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.CerrarSesionAdmin.setOnClickListener{
+            firebaseAuth.signOut()
+            startActivity(Intent(context, Eligir_rol::class.java))
+            activity?.finishAffinity()
+        }
     }
 
 }
