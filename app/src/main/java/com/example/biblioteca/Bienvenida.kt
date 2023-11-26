5ad3de83d043a4d4c0b0d053b2f51dcd15fada76
PedroJimenez <pedro.jimenezli@anahuac.mx>
2023-11-26 10:06:22 -0600

    Mantener sesion activa del admin

--- app/src/main/java/com/example/biblioteca/Bienvenida.kt
+++ app/src/main/java/com/example/biblioteca/Bienvenida.kt
@@ -4,11 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class Bienvenida : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bienvenida)
+        firebaseAuth = FirebaseAuth.getInstance()
         VerBienvenida()
     }
 
@@ -19,13 +29,34 @@ class Bienvenida : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                //Vamos hacia el MainActivity
-                val intent = Intent(this@Bienvenida, Eligir_rol::class.java)
-                startActivity(intent)
-                finishAffinity()
+                ComprobarSesion()
             }
 
         }.start()
+    }
+
+    fun ComprobarSesion(){
+        val firebaseUser = firebaseAuth.currentUser
+        if(firebaseUser == null){
+            startActivity(Intent(this, Eligir_rol::class.java))
+            finishAffinity()
+        }
+        else{
+            val reference = FirebaseDatabase.getInstance().getReference("Usuarios")
+            reference.child(firebaseUser.uid).addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val rol = snapshot.child("rol").value
+                    if(rol == "admin"){
+                        startActivity(Intent(this@Bienvenida, MainActivity::class.java))
+                        finishAffinity()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
+        }
     }
 
 }
--- app/src/main/java/com/example/biblioteca/MainActivity.kt
+++ app/src/main/java/com/example/biblioteca/MainActivity.kt
@@ -1,19 +1,24 @@
 package com.example.biblioteca
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.biblioteca.Fragmentos_Admin.Fragment_Admin_cuenta
 import com.example.biblioteca.Fragmentos_Admin.Fragment_admin_dashboard
 import com.example.biblioteca.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var firebaseAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        firebaseAuth = FirebaseAuth.getInstance()
+        ComprobarSesion()
         VerFragmentoDashboard()
 
         binding.BottomNvAdmin.setOnItemSelectedListener { item ->
@@ -51,5 +56,16 @@ class MainActivity : AppCompatActivity() {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(binding.FragmentsAdmin.id, fragment, "Fragment mi cuenta")
         fragmentTransaction.commit()
+    }
+
+    private fun ComprobarSesion(){
+        val firebaseUser = firebaseAuth.currentUser
+        if(firebaseUser == null){
+            startActivity(Intent(this, Eligir_rol::class.java))
+            finishAffinity()
+        }
+        else{
+            Toast.makeText(applicationContext, "Bienvenido ${firebaseUser.email}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
