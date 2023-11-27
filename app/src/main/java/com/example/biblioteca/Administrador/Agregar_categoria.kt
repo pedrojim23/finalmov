ed7887ce83de6444d6dcce9d3f7f0fa4f50e346f
PedroJimenez <pedro.jimenezli@anahuac.mx>
2023-11-27 13:23:59 -0600

    funciona agregar categoria

--- app/src/main/java/com/example/biblioteca/Administrador/Agregar_categoria.kt
+++ app/src/main/java/com/example/biblioteca/Administrador/Agregar_categoria.kt
@@ -1,12 +1,75 @@
 package com.example.biblioteca.Administrador
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.biblioteca.R
+import com.example.biblioteca.databinding.ActivityAgregarCategoriaBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class Agregar_categoria : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAgregarCategoriaBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_agregar_categoria)
+        binding = ActivityAgregarCategoriaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor...")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.IbRegresar.setOnClickListener{
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        binding.AgregarCategoriaBd.setOnClickListener{
+            ValidarDatos()
+        }
+
+    }
+
+    private var categoria = ""
+    private fun ValidarDatos() {
+        categoria = binding.EtCategoria.text.toString().trim()
+        if(categoria.isEmpty()){
+            Toast.makeText(applicationContext, "Ingrese una categorria", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            AgregarCategoriaBD()
+        }
+    }
+
+    private fun AgregarCategoriaBD() {
+        progressDialog.setMessage("Agregando categoria")
+        progressDialog.show()
+
+        val tiempo = System.currentTimeMillis()
+
+        val hashMap = HashMap<String, Any>()
+        hashMap["id"] = tiempo
+        hashMap["categoria"] = categoria
+        hashMap["tiempo"] = tiempo
+        hashMap["uid"] = "${firebaseAuth.uid}"
+
+        val ref = FirebaseDatabase.getInstance().getReference("Categorias")
+        ref.child("$tiempo")
+            .setValue(hashMap)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                Toast.makeText(applicationContext, "Categoria agregada a la base de datos",Toast.LENGTH_SHORT).show()
+                binding.EtCategoria.setText("")
+            }
+            .addOnFailureListener{e->
+                progressDialog.dismiss()
+                Toast.makeText(applicationContext, "La categoria no se pudo agregar debido a ${e.message}",Toast.LENGTH_SHORT).show()
+            }
     }
 }
