1ac616681061825eec884b6befc06c984281be89
PedroJimenez <pedro.jimenezli@anahuac.mx>
2023-11-27 12:51:14 -0600

    UI agregar categoria

---
+++ app/src/main/java/com/example/biblioteca/Administrador/Agregar_categoria.kt
@@ -0,0 +1,12 @@
+package com.example.biblioteca.Administrador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.biblioteca.R
+
+class Agregar_categoria : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_categoria)
+    }
+}
--- app/src/main/java/com/example/biblioteca/Fragmentos_Admin/Fragment_admin_dashboard.kt
+++ app/src/main/java/com/example/biblioteca/Fragmentos_Admin/Fragment_admin_dashboard.kt
@@ -1,19 +1,39 @@
 package com.example.biblioteca.Fragmentos_Admin
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.biblioteca.Administrador.Agregar_categoria
 import com.example.biblioteca.R
+import com.example.biblioteca.databinding.FragmentAdminDashboardBinding
 
 class Fragment_admin_dashboard : Fragment() {
 
+    private lateinit var binding: FragmentAdminDashboardBinding
+    private lateinit var mContext : Context
+
+    override fun onAttach(context: Context) {
+        mContext = context
+        super.onAttach(context)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_dashboard, container, false)
+        binding = FragmentAdminDashboardBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.BtnAgregarCategoria.setOnClickListener{
+            startActivity(Intent(mContext, Agregar_categoria::class.java))
+        }
+        binding.AgregarPdf.setOnClickListener {
+
+        }
     }
 
 }
