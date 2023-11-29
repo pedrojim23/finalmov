fd8c339675b84d5ad6b767c276cb27c5e66e56eb
PedroJimenez <pedro.jimenezli@anahuac.mx>
2023-11-29 13:58:45 -0600

    ver detalles librp

--- app/src/main/java/com/example/biblioteca/Administrador/DetalleLibro.kt
+++ app/src/main/java/com/example/biblioteca/Administrador/DetalleLibro.kt
@@ -3,10 +3,68 @@ package com.example.biblioteca.Administrador
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.biblioteca.R
+import com.example.biblioteca.databinding.ActivityDetalleLibroBinding
+import com.example.biblioteca.databinding.ItemLibroAdminBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class DetalleLibro : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetalleLibroBinding
+    private var idLibro=""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detalle_libro)
+        binding = ActivityDetalleLibroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        idLibro = intent.getStringExtra("idLibro")!!
+
+        binding.IbRegresar.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        cargarDetalleLibro()
+    }
+
+    private fun cargarDetalleLibro() {
+        val ref = FirebaseDatabase.getInstance().getReference("Libros")
+        ref.child(idLibro)
+            .addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    //obtener info del libro por id
+                    val categoria = "${snapshot.child("categoria").value}"
+                    val contadorDescargas = "${snapshot.child("contadorDescargas").value}"
+                    val contadorVistas = "${snapshot.child("contadorVistas").value}"
+                    val descripcion = "${snapshot.child("descripcion").value}"
+                    val tiempo = "${snapshot.child("tiempo").value}"
+                    val titulo = "${snapshot.child("titulo").value}"
+                    val url = "${snapshot.child("url").value}"
+
+                    //formato tiempo
+                    val fecha = MisFunciones.formatoTiempo(tiempo.toLong())
+                    //cargar cate
+                    MisFunciones.CargarCategoria(categoria, binding.categoriaD)
+                    //cargar miniatura
+                    MisFunciones.CargarPdfUrl("$url", "$titulo", binding.VisualizadorPdf, binding.progressBar, binding.paginasD)
+                    //cargar tamanio
+                    MisFunciones.CargarTamanioPdf("$url", "$titulo", binding.tamanioD)
+
+
+                    //Set info rest
+                    binding.tituloLibroD.text = titulo
+                    binding.descripcionD.text = descripcion
+                    binding.vistasD.text = contadorVistas
+                    binding.descargasD.text = contadorDescargas
+                    binding.fechaD.text = fecha
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
     }
 }
