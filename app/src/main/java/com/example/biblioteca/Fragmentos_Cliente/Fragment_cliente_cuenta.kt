2d05e66e0cc256227d4f1e1e8f12d729c53cc746
PedroJimenez <pedro.jimenezli@anahuac.mx>
2023-11-30 14:40:22 -0600

    mostrar info cl

--- app/src/main/java/com/example/biblioteca/Fragmentos_Cliente/Fragment_cliente_cuenta.kt
+++ app/src/main/java/com/example/biblioteca/Fragmentos_Cliente/Fragment_cliente_cuenta.kt
@@ -7,11 +7,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.example.biblioteca.Administrador.EditarPerfilAdmin
+import com.example.biblioteca.Administrador.MisFunciones
 import com.example.biblioteca.Eligir_rol
 import com.example.biblioteca.R
 import com.example.biblioteca.databinding.FragmentClienteCuentaBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class Fragment_cliente_cuenta : Fragment() {
 
@@ -37,10 +44,52 @@ class Fragment_cliente_cuenta : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         firebaseAuth = FirebaseAuth.getInstance()
 
+        cargarInformacion()
+
         binding.CerrarSesionCliente.setOnClickListener{
             firebaseAuth.signOut()
             startActivity(Intent(context, Eligir_rol::class.java))
             activity?.finishAffinity()
         }
+    }
+
+    private fun cargarInformacion() {
+        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
+        ref.child("${firebaseAuth.uid}")
+            .addValueEventListener(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val nombres = "${snapshot.child("nombres").value}"
+                    val email = "${snapshot.child("email").value}"
+                    var t_registro = "${snapshot.child("tiempo_registrado").value}"
+                    var edad = "${snapshot.child("edad").value}"
+                    val rol = "${snapshot.child("rol").value}"
+                    val imagen = "${snapshot.child("imagen").value}"
+
+                    if (t_registro == null){
+                        t_registro = "0"
+                    }
+
+                    val formato_fecha = MisFunciones.formatoTiempo(t_registro.toLong())
+
+                    binding.TxtNombresCliente.text = nombres
+                    binding.TxtEmailCliente.text = email
+                    binding.TxtTiempoRegistroCliente.text = formato_fecha
+                    binding.TxtEdadCliente.text = edad
+                    binding.TxtRolCliente.text = rol
+
+                    try {
+                        Glide.with(mContext)
+                            .load(imagen)
+                            .placeholder(R.drawable.ic_img_perfil)
+                            .into(binding.imgPerfilCliente)
+                    }catch (e:Exception){
+                        Toast.makeText(mContext, "${e.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
     }
 }
